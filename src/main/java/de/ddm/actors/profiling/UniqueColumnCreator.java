0c92af43c25aa25f1c5b13f2ad513940cd5b16de
Daniel Engelbach <danielengelbach@Daniels-Air.local.wenjo.de>
2023-12-15 12:08:23 +0100

    map for uniques

--- src/main/java/de/ddm/actors/profiling/UniqueColumnCreator.java
+++ src/main/java/de/ddm/actors/profiling/UniqueColumnCreator.java
@@ -15,6 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UniqueColumnCreator extends AbstractBehavior<UniqueColumnCreator.Message> {
 
@@ -65,7 +66,7 @@ public class UniqueColumnCreator extends AbstractBehavior<UniqueColumnCreator.Me
                 .build();
     }
 
-    private Behavior<UniqueColumnCreator.Message> handle(UniqueColumnCreator.CreateUniqueColumnMessage message) {
+    private Behavior<UniqueColumnCreator.Message> handle2(UniqueColumnCreator.CreateUniqueColumnMessage message) {
         double size = this.task.getData().size();
         double redundancies = 0.0;
         double current = 0.0;
@@ -99,6 +100,46 @@ public class UniqueColumnCreator extends AbstractBehavior<UniqueColumnCreator.Me
             }
         }
         message.getWorker().tell(new DependencyWorker.UniqueColumnResultMessage(this.getContext().getSelf(), result, this.task.getTableIndex(),
+                this.task.getColumnIndex(), this.task.id));
+
+        return this;
+    }
+
+    private Behavior<UniqueColumnCreator.Message> handle(UniqueColumnCreator.CreateUniqueColumnMessage message) {
+        double size = this.task.getData().size();
+        double redundancies = 0.0;
+        double current = 0.0;
+        double lastLogTime = System.currentTimeMillis();
+        boolean checkReached = false;
+
+        HashMap<String, Boolean> map = new HashMap<>();
+        for (String s: this.task.getData()){
+            if(map.get(s) == null){
+                map.put(s, true);
+            } else {
+                redundancies += 1;
+            }
+            current += 1;
+
+            // log
+            if(System.currentTimeMillis() - lastLogTime >= 1000) {
+                this.getContext().getLog().info("table "+this.task.getTableIndex()+", column "+this.task.getColumnIndex()+": "+ (Math.round(current / size * 10000.0) / 100.0)+"% checked - redundancy quote: "+(Math.round(redundancies / current * 10000.0) / 100.0)+"%");
+                lastLogTime = System.currentTimeMillis();
+            }
+
+            if(!checkReached && current >= 10000) {
+                checkReached = true;
+                if (redundancies / current >= SystemConfiguration.redundancyThreshold)
+                    this.getContext().getLog().info("the first 10k values contain at least 20% redundant values - continue search for uniques");
+                else {
+                    this.getContext().getLog().info("the first 10k of values contain less than 20% redundant values - aborting search for uniques");
+                    message.getWorker().tell(new DependencyWorker.UniqueColumnResultMessage(this.getContext().getSelf() , this.task.getData(), this.task.getTableIndex(),
+                            this.task.getColumnIndex(), this.task.id));
+                    return this;
+                }
+            }
+        }
+        message.getWorker().tell(new DependencyWorker.UniqueColumnResultMessage(this.getContext().getSelf(), new ArrayList<>(map.keySet()), this.task.getTableIndex(),
                 this.task.getColumnIndex(), this.task.id));
 
         return this;
